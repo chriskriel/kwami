50f8ba748a97ee86f6ae86ea8a531830ca97e8c3
Chris Kriel <chris.kriel@bcx.co.za>
2017-04-13 11:09:01 +0200

    master: testing js modules

--- kwami-generic-webapp/src/main/webapp/scripts/utils.js
+++
@@ -1,9 +0,0 @@
-var Utils = (function () {
-    function Utils() {
-    }
-    Utils.makeDivFromString = function (html) {
-        return (new DOMParser().parseFromString(html, "text/html").body.firstChild);
-    };
-    return Utils;
-}());
-Utils.debug = false;
