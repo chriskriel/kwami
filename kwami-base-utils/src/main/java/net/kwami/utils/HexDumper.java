5a3a10a58a450b5620f710ad7cc56ce9ab214dae
chris <chris.kriel@bcx.co.za>
2017-08-17 17:40:12 +0200

    master: add HexDumper to kwami-base-utils, checkpoint for tcp-proxy

---
+++ kwami-base-utils/src/main/java/net/kwami/utils/HexDumper.java
@@ -0,0 +1,82 @@
+package net.kwami.utils;
+
+public abstract class HexDumper {
+	private static final String HEX = "0123456789ABCDEF";
+	private int hexPerLine = 32;
+	
+	public HexDumper() {
+		super();
+	}
+	
+	public HexDumper(int hexPerLine) {
+		super();
+		this.hexPerLine = hexPerLine;
+	}
+
+	public StringBuilder buildHexDump(byte[] bytes) {
+		return buildHexDump(bytes, bytes.length);
+	}
+
+	public StringBuilder buildHexDump(byte[] inBytes, int length) {
+		byte[] bytes = new byte[length];
+		System.arraycopy(inBytes, 0, bytes, 0, length);
+		StringBuilder sb = new StringBuilder((length == 0 ? 20 : length) * 2);
+		sb.append('\n');
+		if (length == 0) {
+			sb.append("no bytes to display in hex format");
+			return sb;
+		}
+		int i;
+		for (i = 0; i < length; i++) {
+			if (i > 0) {
+				if (i % 4 == 0) {
+					sb.append(' ');
+				}
+				if (i % hexPerLine == 0) {
+					sb.append("| ");
+					sb.append(removeNonPrintable(bytes, i - hexPerLine, hexPerLine));
+					sb.append(" |\n");
+				}
+			}
+			translateToHex(sb, bytes[i]);
+		}
+		// now complete the final line
+		int j = i % hexPerLine;
+		if (j == 0) {
+			sb.append(" | ");
+			sb.append(removeNonPrintable(bytes, i - hexPerLine, hexPerLine));
+			sb.append(" |");
+		} else {
+			for (; i % hexPerLine != 0; i++) {
+				if (i % 4 == 0) {
+					sb.append(' ');
+				}
+				sb.append("  ");
+			}
+			if (j > 0) {
+				sb.append(" | ");
+				sb.append(removeNonPrintable(bytes, length - j, j));
+			}
+		}
+		return sb;
+	}
+
+	private void translateToHex(StringBuilder sb, byte b) {
+		int byteValue = b;
+		int leftDigit = byteValue & 0x000000F0;
+		leftDigit >>= 4;
+		int rightDigit = byteValue & 0x0000000F;
+		sb.append(HEX.charAt(leftDigit));
+		sb.append(HEX.charAt(rightDigit));
+	}
+
+	private String removeNonPrintable(byte[] bytes, int offset, int length) {
+		if (bytes.length == 0)
+			return "";
+		for (int i = 0; i < bytes.length; i++) {
+			if (bytes[i] < 32 || bytes[i] > 126)
+				bytes[i] = 32;
+		}
+		return new String(bytes, offset, length);
+	}
+}
--- tcp-proxy/src/main/java/net/kwami/TcpProxy.java
+++ tcp-proxy/src/main/java/net/kwami/TcpProxy.java
@@ -1,111 +1,105 @@
-import java.io.*;
-import java.net.*;
+package net.kwami;
 
-public class TcpProxy {
-  public static void main(String[] args) throws IOException {
-    try {
-      String host = "your Proxy Server";
-      int remoteport = 100;
-      int localport = 111;
-      // Print a start-up message
-      System.out.println("Starting proxy for " + host + ":" + remoteport
-          + " on port " + localport);
-      // And start running the server
-      runServer(host, remoteport, localport); // never returns
-    } catch (Exception e) {
-      System.err.println(e);
-    }
-  }
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
-  /**
-   * runs a single-threaded proxy server on
-   * the specified local port. It never returns.
-   */
-  public static void runServer(String host, int remoteport, int localport)
-      throws IOException {
-    // Create a ServerSocket to listen for connections with
-    ServerSocket ss = new ServerSocket(localport);
+public class TcpProxy extends Thread {
+	private String proxyChannel;
+	private int localPort;
+	private String remoteHost;
+	private int remotePort;
 
-    final byte[] request = new byte[1024];
-    byte[] reply = new byte[4096];
+	public TcpProxy(String proxyChannel, int localPort, String remoteHost, int remotePort) {
+		super();
+		this.proxyChannel = proxyChannel;
+		this.localPort = localPort;
+		this.remoteHost = remoteHost;
+		this.remotePort = remotePort;
+	}
 
-    while (true) {
-      Socket client = null, server = null;
-      try {
-        // Wait for a connection on the local port
-        client = ss.accept();
+	@Override
+	public void run() {
+		try (ServerSocket ss = new ServerSocket(localPort)) {
+			final byte[] request = new byte[4096];
+			byte[] response = new byte[4096];
+			while (true) {
+				try (Socket localSocket = ss.accept();
+						Socket remoteServer = new Socket(remoteHost, remotePort);
+						final InputStream streamFromClient = localSocket.getInputStream();
+						final OutputStream streamToServer = remoteServer.getOutputStream();
+						final InputStream streamFromServer = remoteServer.getInputStream();
+						final OutputStream streamToClient = localSocket.getOutputStream()) {
+					Thread sendingThread = new Thread() {
+						public void run() {
+							int bytesRead;
+							try {
+								while ((bytesRead = streamFromClient.read(request)) != -1) {
+									streamToServer.write(request, 0, bytesRead);
+								}
+								streamToServer.flush();
+							} catch (IOException e) {
+							}
+							try {
+								streamToServer.close();
+							} catch (IOException e) {
+							}
+						}
+					};
+					sendingThread.setDaemon(true);
+					sendingThread.setName("sendingThread[" + proxyChannel + "]");
+					sendingThread.start();
+					int bytesRead;
+					try {
+						while ((bytesRead = streamFromServer.read(response)) != -1) {
+							streamToClient.write(response, 0, bytesRead);
+						}
+						streamToClient.flush();
+					} catch (IOException e) {
+					}
+					streamToClient.close();
+				} catch (IOException e) {
+					System.out.print("Streams to and from proxy failed\n ");
+					e.printStackTrace();
+				}
+			}
+		} catch (IOException e) {
+			System.out.print("ServerSocket open failed\n ");
+			e.printStackTrace();
+		}
+	}
 
-        final InputStream streamFromClient = client.getInputStream();
-        final OutputStream streamToClient = client.getOutputStream();
-
-        // Make a connection to the real server.
-        // If we cannot connect to the server, send an error to the
-        // client, disconnect, and continue waiting for connections.
-        try {
-          server = new Socket(host, remoteport);
-        } catch (IOException e) {
-          PrintWriter out = new PrintWriter(streamToClient);
-          out.print("Proxy server cannot connect to " + host + ":"
-              + remoteport + ":\n" + e + "\n");
-          out.flush();
-          client.close();
-          continue;
-        }
-
-        // Get server streams.
-        final InputStream streamFromServer = server.getInputStream();
-        final OutputStream streamToServer = server.getOutputStream();
-
-        // a thread to read the client's requests and pass them
-        // to the server. A separate thread for asynchronous.
-        Thread t = new Thread() {
-          public void run() {
-            int bytesRead;
-            try {
-              while ((bytesRead = streamFromClient.read(request)) != -1) {
-                streamToServer.write(request, 0, bytesRead);
-                streamToServer.flush();
-              }
-            } catch (IOException e) {
-            }
-
-            // the client closed the connection to us, so close our
-            // connection to the server.
-            try {
-              streamToServer.close();
-            } catch (IOException e) {
-            }
-          }
-        };
-
-        // Start the client-to-server request thread running
-        t.start();
-
-        // Read the server's responses
-        // and pass them back to the client.
-        int bytesRead;
-        try {
-          while ((bytesRead = streamFromServer.read(reply)) != -1) {
-            streamToClient.write(reply, 0, bytesRead);
-            streamToClient.flush();
-          }
-        } catch (IOException e) {
-        }
-
-        // The server closed its connection to us, so we close our
-        // connection to our client.
-        streamToClient.close();
-      } catch (IOException e) {
-        System.err.println(e);
-      } finally {
-        try {
-          if (server != null)
-            server.close();
-          if (client != null)
-            client.close();
-        } catch (IOException e) {
-        }
-      }
-    }
-  }
+	public static void main(String[] args) throws IOException {
+		int remotePort = 0, localPort = 0;
+		String remoteHost = "localhost";
+		if (args.length == 0) {
+			System.out.println("Usage: net.kwami.TcpProxy <localPort:remoteHost:remotePort> ...");
+			return;
+		}
+		for (String proxyChannel : args) {
+			boolean valid = true;
+			String[] addressParts = proxyChannel.split(":");
+			if (addressParts.length != 3)
+				valid = false;
+			try {
+				localPort = Integer.parseInt(args[0]);
+				remoteHost = args[1];
+				remotePort = Integer.parseInt(args[2]);
+			} catch (Throwable t) {
+				valid = false;
+			}
+			if (!valid) {
+				System.out.printf("Expecting, for example, 80:remoteHost:80 but got %s (trying the next parameter).\n",
+						proxyChannel);
+				continue;
+			}
+			System.out.printf("Starting a proxy for %s\n", proxyChannel);
+			TcpProxy proxy = new TcpProxy(proxyChannel, localPort, remoteHost, remotePort);
+			proxy.setName("ReceivingThread[" + proxyChannel + "]");
+			proxy.setDaemon(true);
+			proxy.start();
+		}
+	}
 }
