d789595131c4bb2d618e105a05df410bb18f4f9d
Chris Kriel <chris.kriel@bcx.co.za>
2019-03-27 20:11:01 +0200

    master: added method for alert logging

---
+++ kwami-log4j2-uber/src/main/java/net/kwami/utils/LogUtils.java
@@ -0,0 +1,13 @@
+package net.kwami.utils;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LogUtils {
+	private static final Logger ALERTS = LogManager.getLogger("alerts.log");
+	
+	public static final void ALERT(String simpleClassName, int alertNumber, String alertMessage) {
+		ALERTS.info("{}-{}: {}", simpleClassName, alertNumber, alertMessage);
+	}
+
+}
